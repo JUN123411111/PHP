24ed7e6f1759f285ae45f32141a6f51cdb620f6a
陳俊良 <a0900672568@gmail.com>
2026-04-26 15:24:31 +0800

    Update user.php

--- PHP/user.php
+++ PHP/user.php
@@ -3,10 +3,10 @@
     <body>
     <?php
         error_reporting(0);
-        session_start();
+        session_start(); // 啟動 session
         if (!$_SESSION["id"]) {
-            echo "請登入帳號";
-            echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+            echo "請登入帳號"; // 檢查是否登入
+            echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";// 3秒後跳轉登入頁
         }
         else{   
             echo "<h1>使用者管理</h1>
@@ -14,9 +14,11 @@
                 <table border=1>
                     <tr><td></td><td>帳號</td><td>密碼</td></tr>";
             
-            $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+            $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");// 連接資料庫
             $result=mysqli_query($conn, "select * from user");
+            // 逐筆顯示資料
             while ($row=mysqli_fetch_array($result)){
+                // 修改 & 刪除連結（帶入 id）
                 echo "<tr><td><a href=19.user_edit_form.php?id={$row['id']}>修改</a>||<a href=17.user_delete.php?id={$row['id']}>刪除</a></td><td>{$row['id']}</td><td>{$row['pwd']}</td></tr>";
             }
             echo "</table>";
