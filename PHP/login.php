cc7a423690c9a7b396cffb20fe6ff726d4cf3896
陳俊良 <a0900672568@gmail.com>
2026-04-26 15:10:42 +0800

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -7,17 +7,18 @@
    $login=FALSE;
    while ($row=mysqli_fetch_array($result)) {
      if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
-       $login=TRUE;
+       $login=TRUE;// 如果符合，登入成功
      }
    } 
+// 判斷是否登入成功
    if ($login==TRUE){
-    session_start();
-    $_SESSION["id"]=$_POST["id"];
-     echo "歡迎登入";
-    echo"<meta http-equiv=REFRESH content='0,bulletin.php'>";
+    session_start();// 啟動 session（用來記錄登入狀態）
+    $_SESSION["id"]=$_POST["id"]; // 將使用者帳號存入 session
+     echo "歡迎登入";// 顯示登入成功訊息
+    echo"<meta http-equiv=REFRESH content='0,bulletin.php'>";// 立即跳轉到 bulletin.php 頁面
    }
   else{
-     echo "帳號/密碼 錯誤";
-     echo"<meta http-equiv=REFRESH content='3,bulletin.php'>";
+     echo "帳號/密碼 錯誤";// 登入失敗提示
+     echo"<meta http-equiv=REFRESH content='3,bulletin.php'>";// 3秒後跳轉
   }
 ?>
