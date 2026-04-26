29ca11df4c49fe2ebd807e979bc9c1f3c5956f2a
陳俊良 <a0900672568@gmail.com>
2026-04-26 15:11:57 +0800

    Update logout.php

--- PHP/logout.php
+++ PHP/logout.php
@@ -1,8 +1,15 @@
 <?php
 
+    // 啟動 session（要操作 session 一定要先啟動）
     session_start();
+
+    // 清除 session 中的 id（代表登出）
     unset($_SESSION["id"]);
+
+    // 顯示登出成功訊息
     echo "登出成功";
-    echo "<meta http-equiv=REFRESH content='3,login.html'>"
+
+    // 3秒後自動跳轉回登入頁面
+    echo "<meta http-equiv=REFRESH content='3,login.html'>";
 
 ?>
