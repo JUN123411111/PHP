0521d3ae195c7c738a72f893448e9942c8a25646
陳俊良 <a0900672568@gmail.com>
2026-04-26 15:12:17 +0800

    Update session.php

--- PHP/session.php
+++ PHP/session.php
@@ -1,8 +1,12 @@
 <?php
 
-    session_start();//seaaion啟動
-    $_SESSION["userName"]="Mary";
+    // 啟動 session（要使用 $_SESSION 一定要先啟動）
+    session_start(); // session 啟動（你原本寫 seaaion，有拼錯）
 
+    // 在 session 中存入一個變數 userName，值為 Mary
+    $_SESSION["userName"] = "Mary";
+
+    // 輸出 session 中的 userName
     echo $_SESSION["userName"];
 
 ?>
