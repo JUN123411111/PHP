87475fdce0d2c9b005e579e73df0144486687b7f
陳俊良 <a0900672568@gmail.com>
2026-06-15 15:04:01 +0800

    Refactor logout.php for improved readability

--- logout.php
+++ logout.php
@@ -1,7 +1,10 @@
 <?php
+    // 啟動 Session
     session_start();
+    // 刪除 Session 中儲存的登入帳號資料
     unset($_SESSION["id"]);
+    // 顯示登出成功訊息
     echo "登出成功....";
+    // 3 秒後自動返回登入頁面
     echo "<meta http-equiv=REFRESH content='3; url=login.html'>";
-
 ?>
