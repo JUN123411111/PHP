ca211432fb5b12ce854b80ceb4f0beef6a586cb3
陳俊良 <a0900672568@gmail.com>
2026-06-15 15:06:07 +0800

    Update user_add_form.php

--- user_add_form.php
+++ user_add_form.php
@@ -2,13 +2,19 @@
     <head><title>新增使用者</title></head>
     <body>
 <?php        
+    // 關閉錯誤訊息顯示
     error_reporting(0);
+    // 啟動 Session
     session_start();
+    // 檢查使用者是否已登入
     if (!$_SESSION["id"]) {
+        // 未登入時顯示提示訊息
         echo "請登入帳號";
+        // 3 秒後跳轉至登入頁面
         echo "<meta http-equiv=REFRESH content='3, url=login.html'>";
     }
     else{    
+        // 顯示新增使用者表單
         echo "
             <form action=user_add.php method=post>
                 帳號：<input type=text name=id><br>
