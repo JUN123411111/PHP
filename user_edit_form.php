2244c3ccebe2c5e620f25f73691349473adcd465
陳俊良 <a0900672568@gmail.com>
2026-06-15 15:08:02 +0800

    Update user_edit_form.php

--- user_edit_form.php
+++ user_edit_form.php
@@ -2,16 +2,25 @@
     <head><title>修改使用者</title></head>
     <body>
     <?php
+    // 關閉錯誤訊息顯示
     error_reporting(0);
+    // 啟動 Session
     session_start();
+    // 檢查使用者是否已登入
     if (!$_SESSION["id"]) {
+        // 未登入時顯示提示訊息
         echo "請登入帳號";
+        // 3 秒後跳轉至登入頁面
         echo "<meta http-equiv=REFRESH content='3, url=login.html'>";
     }
     else{   
+        // 連接資料庫
         $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+        // 根據網址傳來的 id 查詢指定使用者資料
         $result=mysqli_query($conn, "select * from user where id='{$_GET['id']}'");
+        // 取得查詢結果
         $row=mysqli_fetch_array($result);
+        // 顯示修改使用者表單，並帶入原有資料
         echo "
         <form method=post action=user_edit.php>
             <input type=hidden name=id value={$row['id']}>
